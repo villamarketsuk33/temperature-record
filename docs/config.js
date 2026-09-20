55532744d22619b5886db0d305f124f72a7c7236
villamarketsuk33 <villamarketsuk.33@gmail.com>
2026-09-20 14:22:48 +0700

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,5 @@
+﻿// ใส่เฉพาะลิงก์ Apps Script Web App ที่ลงท้าย /exec
+// ห้ามใส่รหัสเข้าใช้งานสาขาหรือ API key ในไฟล์นี้หรือใน GitHub
+window.TEMPERATURE_CONFIG = Object.freeze({
+  apiUrl: 'https://script.google.com/macros/s/AKfycbw0y--aUjlwZoB_TpYwjRePIOs6uNhJs3BM29k1u5ObOHhEqJJe0aGemy4i5Y8nW-mDsw/exec'
+});
